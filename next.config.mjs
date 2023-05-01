71df345db5637e30f94e4cd344c0493220c9fd4a
microservicesRagatex <microservices@ragatex.com>
2023-05-01 17:08:44 +0200

    yh

--- next.config.mjs
+++ next.config.mjs
@@ -18,5 +18,14 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+    ],
+  },
 };
 export default config;
